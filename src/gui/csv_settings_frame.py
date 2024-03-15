3f6fc1c59fe1a587c1b777f24e1915e30adfce94
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-03-15 14:48:42 -0700

    style: Reactor CsvSettingsFrame class
    This commit does not introduce any functional changes.
    
    - Refactor CsvSettingsFrame to allow easier readability and improve extensability.

--- src/gui/csv_settings_frame.py
+++ src/gui/csv_settings_frame.py
@@ -6,50 +6,51 @@ class CsvSettingsFrame(ctk.CTkFrame):
     def __init__(self, master, font, values):
         super().__init__(master)
         self.values = values
-        title = "CSV Settings"
-        title_label = ctk.CTkLabel(self, text=title, font=(font, 18))
+        self.font = font
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.create_title_label()
+        self.create_output_path_frame()
+        self.create_update_csv_frame()
+
+    def create_title_label(self):
+        title_label = ctk.CTkLabel(self, text="CSV Settings", font=(self.font, 18))
         title_label.pack(pady=(10, 20))
 
-        # Frame for CSV output path
+    def create_output_path_frame(self):
         output_path_frame = ctk.CTkFrame(self, fg_color='transparent')
         output_path_frame.pack(anchor='center')
-        
-        selected_label = ctk.CTkLabel(output_path_frame, text='Output Path')
-        selected_label.pack(side='left', padx=5, pady=(0,10))
-        outline_frame = ctk.CTkFrame(output_path_frame, corner_radius=10, fg_color="black")
-        outline_frame.pack(side='left', padx=5, pady=(0,10))
 
-        self.current_path_label = ctk.CTkLabel(outline_frame, 
-                                          font=font,
-                                          text= self.values['csv_output_path'])
+        selected_label = ctk.CTkLabel(output_path_frame, text='Output Path')
+        selected_label.pack(side='left', padx=5, pady=(0, 10))
+
+        outline_frame = ctk.CTkFrame(output_path_frame, corner_radius=10, fg_color="black")
+        outline_frame.pack(side='left', padx=5, pady=(0, 10))
+
+        self.current_path_label = ctk.CTkLabel(outline_frame, font=self.font, text=self.values['csv_output_path'])
         self.current_path_label.pack(side='left', padx=10, pady=5)
 
-        browse_button = ctk.CTkButton(output_path_frame, 
-                                      font=font, 
-                                      text='Browse',
-                                      command=self.browse_folder)
-        browse_button.pack(side='left', padx=10, pady=(0,10))
+        browse_button = ctk.CTkButton(output_path_frame, font=self.font, text='Browse', command=self.browse_folder)
+        browse_button.pack(side='left', padx=10, pady=(0, 10))
 
-        # Frame for updating CSV upon completion
-        frame = ctk.CTkFrame(self, fg_color='transparent')
-        frame.pack(anchor="center")
-        self.update_csv_upon_completion_checkbox = ctk.CTkCheckBox(frame, 
-                                                              onvalue=True,
-                                                              offvalue=False,
-                                                              font=font,
-                                                              text="Update CSV on completion",
-                                                              command=self.update_csv_on_completion
-                                                              )
+    def create_update_csv_frame(self):
+        update_csv_frame = ctk.CTkFrame(self, fg_color='transparent')
+        update_csv_frame.pack(anchor="center")
+
+        self.update_csv_upon_completion_checkbox = ctk.CTkCheckBox(update_csv_frame, onvalue=True, offvalue=False,
+                                                                   font=self.font, text="Update CSV on completion",
+                                                                   command=self.update_csv_on_completion)
         if self.values['update_csv_on_completion']:
             self.update_csv_upon_completion_checkbox.select()
-        
-        self.update_csv_upon_completion_checkbox.pack(side="left", padx=10, pady=(0,10))
+        self.update_csv_upon_completion_checkbox.pack(side="left", padx=10, pady=(0, 10))
 
     def browse_folder(self):
         folder_path = filedialog.askdirectory()
         if folder_path:
-            update_config_field('config.json', 'csv_settings.csv_output_path', f"{folder_path}/data.csv")
-            self.current_path_label.configure(text=f"{folder_path}/data.csv")
+            new_path = f"{folder_path}/data.csv"
+            update_config_field('config.json', 'csv_settings.csv_output_path', new_path)
+            self.current_path_label.configure(text=new_path)
 
     def update_csv_on_completion(self):
         checkbox_value = self.update_csv_upon_completion_checkbox.get()
