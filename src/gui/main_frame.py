7c2667989f08cd8e907e51597a4c989d8953233e
henrytrieu88@gmail.com <henrytrieu88@gmail.com>
2024-03-15 14:24:58 -0700

    style: Refactor MainFrame class
    This commit does not introduce any functional changes.
    
    - Further simplified the MainFrame GUI code to improve readability.

--- src/gui/main_frame.py
+++ src/gui/main_frame.py
@@ -111,6 +111,84 @@ class MainFrame(ctk.CTk):
             button_frame, text='Start', text_color="#008000", fg_color='#4dff4d', hover_color='#3cb043', command=self.toggle_start_stop)
         self.start_stop_button.pack(side='left', padx=(5, 20), pady=(20, 10))
 
+    def toggle_start_stop(self):
+        if self.start_stop_button.cget('text') == 'Start':
+            self.disable_frames()
+            self.start_stop_button.configure(text='Stop', text_color='white', fg_color='#ff4d4d', hover_color='#ff8080')
+            self.begin_scraping()
+        elif self.start_stop_button.cget('text') == 'Stop':
+            self.stop_scraping = True
+            self.enable_frames()
+            self.start_stop_button.configure(text='Start', text_color="#008000", fg_color='#4dff4d', hover_color='#3cb043')
+
+    def toggle_scrape_all_checkbox(self):
+        if self.scrape_all_checkbox.get() == 0:
+            # Configured to scrape a specific num of pages
+            self.num_pages_to_scrape_entry_field.configure(state=ctk.NORMAL, fg_color='#343638')
+            self.num_pages_to_scrape_entry_field.delete(0, len(self.num_pages_to_scrape_entry_field.get()))
+            self.num_pages_to_scrape_entry_field.insert(0, 5)
+            update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=5)
+        else:
+            # Configured to scrape all pages
+            self.num_pages_to_scrape_entry_field.delete(0, len(self.num_pages_to_scrape_entry_field.get()))
+            self.num_pages_to_scrape_entry_field.configure(state=ctk.DISABLED, fg_color='#A0A0A0')
+            update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=0)
+
+    def update_config_num_pages_scrape(self, event):
+        new_value = int(self.num_pages_to_scrape_entry_field.get())
+        update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=new_value)
+
+    def begin_scraping(self):
+        self.stop_scraping = False
+        self.scraping_thread = threading.Thread(target=self.run_scraper)
+        self.scraping_thread.start()
+
+    def setup_scraper(self):
+        indeed_criteria = self.config['indeed_criteria']
+        indeed_url = utils.build_indeed_url(
+            max_days_posted_ago=indeed_criteria['max_days_posted_ago'],
+            position=indeed_criteria['position'],
+            experience_level=indeed_criteria['experience_level'],
+            job_type=indeed_criteria['job_type'],
+            location=indeed_criteria['location']
+        )
+
+        return Scraper(indeed_url)
+
+    def run_scraper(self):
+        scraper = self.setup_scraper()
+        num_pages_to_scrape = self.config['num_pages_to_scrape']
+        pages_scraped = 0
+
+        while (self.stop_scraping == False) and (num_pages_to_scrape == 0 or pages_scraped < num_pages_to_scrape):
+            extracted_hash_ids = scraper.extract_current_page()
+
+            # Stop parsing when the last page has been parsed twice
+            if self.stop_scraping or extracted_hash_ids == scraper.previous_page_hash_ids:
+                break
+            else:
+                scraper.previous_page_hash_ids = extracted_hash_ids
+                scraper.navigate_next_page()
+                pages_scraped += 1
+
+        print(f"Number of pages scraped: {pages_scraped}")
+        print(f"Number of new records: {len(scraper.jobs) - scraper.initial_num_records}\n")
+
+        scraper.shutdown()
+    
+        if self.config['csv_settings']['update_csv_on_completion']:
+            utils.write_jobs_csv(self.config['csv_settings']['csv_output_path'], scraper.jobs)
+
+        self.enable_frames()
+
+    def disable_frames(self):
+        for frame in self.frames:
+            self.disable_frame(frame)
+
+    def enable_frames(self):
+        for frame in self.frames:
+            self.enable_frame(frame)
+
     def disable_frame(self, frame):
         for child in frame.winfo_children():
             if isinstance(child, ctk.CTkFrame):
@@ -140,84 +218,3 @@ class MainFrame(ctk.CTk):
                 child.configure(state=ctk.NORMAL)
             elif isinstance(child, ctk.CTkButton):
                 child.configure(state=ctk.NORMAL)
-
-    def toggle_start_stop(self):
-        if self.start_stop_button.cget('text') == 'Start':
-            # Disable all widgets before starting to scrape
-            for frame in self.frames:
-                self.disable_frame(frame)
-            self.start_stop_button.configure(text='Stop', text_color='white', fg_color='#ff4d4d', hover_color='#ff8080')
-
-            # Begin scraping
-            self.stop_scraping = False  # Reset the flag
-            self.scraping_thread = threading.Thread(target=self.run_scraper)
-            self.scraping_thread.start()
-
-        elif self.start_stop_button.cget('text') == 'Stop':
-            self.stop_scraping = True  # Reset the flag
-
-            # Re-enable all widgets to allow further configuration if desired
-            for frame in self.frames:
-                self.enable_frame(frame)
-
-            self.start_stop_button.configure(text='Start', text_color="#008000", fg_color='#4dff4d', hover_color='#3cb043')
-
-    def toggle_scrape_all_checkbox(self):
-        if self.scrape_all_checkbox.get() == 0:
-            # Configured to scrape a specific num of pages
-            self.num_pages_to_scrape_entry_field.configure(state=ctk.NORMAL, fg_color='#343638')
-            self.num_pages_to_scrape_entry_field.delete(0, len(self.num_pages_to_scrape_entry_field.get()))
-            self.num_pages_to_scrape_entry_field.insert(0, 5)
-            update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=5)
-        else:
-            # Configured to scrape all pages
-            self.num_pages_to_scrape_entry_field.delete(0, len(self.num_pages_to_scrape_entry_field.get()))
-            self.num_pages_to_scrape_entry_field.configure(state=ctk.DISABLED, fg_color='#A0A0A0')
-            update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=0)
-
-    def update_config_num_pages_scrape(self, event):
-        new_value = int(self.num_pages_to_scrape_entry_field.get())
-        update_config_field(filepath='config.json', field_path='num_pages_to_scrape', new_value=new_value)
-
-    def run_scraper(self):
-        # Load configuration variables scraper
-        with open('config.json') as config_file:
-            config = json.load(config_file)
-
-        indeed_criteria = config['indeed_criteria']
-        indeed_url = utils.build_indeed_url(
-            max_days_posted_ago = indeed_criteria['max_days_posted_ago'],
-            position            = indeed_criteria['position'],
-            experience_level    = indeed_criteria['experience_level'],
-            job_type            = indeed_criteria['job_type'],
-            location            = indeed_criteria['location']
-        )
-
-        num_pages_to_scrape = config['num_pages_to_scrape']
-
-        # Set up and run the Scraper
-        scraper = Scraper(indeed_url)
-        pages_scraped = 0
-
-        while (self.stop_scraping == False) and (num_pages_to_scrape == 0 or pages_scraped < num_pages_to_scrape):
-            extracted_hash_ids = scraper.extract_current_page()
-
-            # Stop parsing when the last page has been parsed twice
-            if self.stop_scraping or extracted_hash_ids == scraper.previous_page_hash_ids:
-                break
-            else:
-                scraper.previous_page_hash_ids = extracted_hash_ids
-                scraper.navigate_next_page()
-                pages_scraped += 1
-
-        print(f"Number of pages scraped: {pages_scraped}")
-        print(f"Number of new records: {len(scraper.jobs) - scraper.initial_num_records}\n")
-
-        scraper.shutdown()
-    
-        if config['csv_settings']['update_csv_on_completion']:
-            utils.write_jobs_csv(config['csv_settings']['csv_output_path'], scraper.jobs)
-
-        # Re-enable all widgets to allow further configuration
-        for frame in self.frames:
-            self.enable_frame(frame)
